c1efec8fe2a3bbc8aca665012d8c81bbb12be4b9
YOadYo06 <Youness06adil@gmail.com>
2025-12-14 21:35:32 +0100

    Disable wayfinder plugin during Vercel builds

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,11 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
+        ...(process.env.VERCEL ? [] : [
+            wayfinder({
+                formVariants: true,
+            })
+        ]),
         vue({
             template: {
                 transformAssetUrls: {
